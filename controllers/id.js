0d5b1c2d989f6fe645a94cd5625e173d087225b0
hudsonmcmanus <hudson.mcmanus@gmail.com>
2022-03-28 11:04:27 -0700

    Added more functionality to the id route, still needs response formatting and change or validator

--- controllers/id.js
+++ controllers/id.js
@@ -4,11 +4,19 @@ const axios = require('axios');
 // TODO: Verify token
 const getId = async (req, res) => {
     // VALIDATE DATA
-    const { error } = idRequestValidation(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+    // const { error } = idRequestValidation(req.body);
+    // if (error) return res.status(400).send(error.details[0].message);
+
+    req.body.country = !req.body.country ? 'english-united-states' : req.body.country;
+    req.body.gender = !req.body.gender ? 'male' : req.body.gender;
+
+    console.log(req.body.country);
+
+    const url = `https://api.namefake.com/${req.body.country}/${req.body.gender}`
     axios({
         method: 'get',
-        url: 'https://api.namefake.com/',
+        url: url,
+        data: req.body,
     })
         .then(idRes => {
             console.log(idRes.data);
