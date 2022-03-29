2e1e9437a0a6833e3bb30ec77194836a5a1d617c
Jason Lui <mrjasonlui@gmail.com>
2022-03-29 14:43:14 -0700

    Add matching external API parameters to

--- controllers/id.js
+++ controllers/id.js
@@ -2,13 +2,25 @@ const { IdRequestValidation } = require('../validation.js');
 
 // TODO: Verify token
 const getId = async (req, res) => {
+
+    country_match = {
+        'usa' : 'english-united-states',
+        'canada' : 'english-canada',
+        'russia' : 'russian-russia',
+        'ukraine' : 'ukranian-ukraine',
+        'poland' : 'polish-poland',
+        'netherlands' : 'dutch-netherlands',
+        'sweden' : 'swedish-sweden',
+        'china' : 'chinese'
+    }
+
     // VALIDATE DATA
     const { error } = IdRequestValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
     extReq = {};
     if (req.body.sex) extReq.sex = req.body.sex;
-    if (req.body.country) extReq.country = req.body.country;
+    if (req.body.country) extReq.country = country_match[req.body.country];
     if (req.body.eye_color) extReq.eye_color = req.body.eye_color;
     if (req.body.hair_color) extReq.hair_color = req.body.hair_color;
     if (req.body.ethnicity) extReq.ethnicity = req.body.ethnicity;
